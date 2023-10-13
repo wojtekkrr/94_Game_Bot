d0e5a70aada8f3e2e0256b99a48499bb45f3085c
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-10-13 15:51:38 +0200

    Działało skakanie na dużej prędkości, z tego ewentualnie brać jump(), a usunąć hide()

--- main.py
+++ main.py
@@ -36,18 +36,45 @@ pyautogui.press('space')
 # print(y)
 
 # Współrzędne pixela do analizy
-x = 470
-y = 690
+x = 510
+y_jump = 710
+y_hide = 641
+
+x_steps = 10
+x_step = 10
+
 
 kolor_poszukiwany = (83, 83, 83)
+
+x_ckeck = []
+for i in range(x_steps + 1):
+    x_ckeck.append(int(x - x_steps/2 * x_step + i * x_step))
+
+
+def jump():
+    for x_cord in x_ckeck:
+        kolor_piksela = pyautogui.pixel(x_cord, y_jump)
+        if kolor_piksela == kolor_poszukiwany:
+            return True
+    return False
+
+
+def hide():
+    for x_cord in x_ckeck:
+        kolor_piksela_dol = pyautogui.pixel(x_cord, y_jump)
+        kolor_piksela_gora = pyautogui.pixel(x_cord, y_hide)
+        if kolor_piksela_dol != kolor_poszukiwany and kolor_piksela_gora == kolor_poszukiwany:
+            return True
+    return False
+
 
 # Pętla while będzie działać dopóki warunek escape_pressed będzie równy False
 escape_pressed = False
 while not escape_pressed:
-    kolor_piksela = pyautogui.pixel(x, y)
-    # print(kolor_piksela)
-    if kolor_piksela == kolor_poszukiwany:
+    if jump():
         pyautogui.press('space')
+    if hide():
+        pyautogui.press('down')
     sleep(0.001)
     if keyboard.is_pressed('esc'):
         escape_pressed = True
